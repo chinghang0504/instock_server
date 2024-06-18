2d092ff9ce84f24df387e0d2df33f630372c7587
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-18 14:01:27 -0700

    create endpoint for warehouselist & export to routes

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -2,3 +2,13 @@ import initKnex from "knex";
 import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
+const warehouselist = async (_req, res) => {
+    try{
+        const data= await knex("warehouses")
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(400).send (`Error retriving warehouse list:${error}`);
+    }
+};
+
+export {warehouselist}
--- routes/warehouse.js
+++ routes/warehouse.js
@@ -3,5 +3,6 @@ import * as userController from "../controllers/warehouse-controller.js";
 
 const router = express.Router();
 
+router.route("/").get(userController.warehouselist);
 
 export default router;
