146ae28ac2a8a2b8d96c1e0f400994b05b264a1d
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-18 14:58:40 -0700

    add ; where needed

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -4,7 +4,7 @@ const knex = initKnex(configuration);
 
 const inventorylist = async (_req, res) => {
     try{
-        const data= await knex("inventories")
+        const data= await knex("inventories");
         res.status(200).json(data)
     } catch (error) {
         res.status(400).send (`Error retriving inventory list:${error}`);
