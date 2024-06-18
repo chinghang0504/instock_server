8cb9b20effd36f01dd770f3673d845fc02b1ce4f
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-18 14:21:37 -0700

    update warehouselist to WarehouseList

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -3,6 +3,6 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 
 const router = express.Router();
 
-router.route("/").get(warehouseController.warehouselist);
+router.route("/").get(warehouseController.warehouseList);
 
 export default router;
