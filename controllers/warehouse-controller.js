9d3a7b0b11912830a15cf7cc834e10af9640ab86
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-18 14:34:05 -0700

    create endpoint for single warehouse. connect controls & route + test

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -12,4 +12,20 @@ const warehouseList = async (_req, res) => {
 };
 
 
-export {warehouseList}
+const warehouseSingle = async (req, res) => {
+    try {
+        const warehouseData = await knex("warehouses").where({ id: req.params.id }).first();
+
+        if (!warehouseData) {
+            return res.status(404).json({ message: `Warehouse with ID: ${req.params.id} not found` });
+        }
+
+        return res.status(200).json(warehouseData);
+    } catch (error) {
+        return res.status(500).json({ message: `Unable to retrieve data for warehouse with ID: ${req.params.id}` });
+    }
+};
+
+
+ 
+export {warehouseList, warehouseSingle}
--- routes/warehouse.js
+++ routes/warehouse.js
@@ -4,5 +4,6 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 const router = express.Router();
 
 router.route("/").get(warehouseController.warehouseList);
+router.route("/:id").get(warehouseController.warehouseSingle);
 
 export default router;
