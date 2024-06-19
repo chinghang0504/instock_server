f394b52049352d1a58130aca5991843a977f9ae4
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-18 21:23:08 -0700

    add endpoint for invertoy by warehouse, connect to coontrols & routes + test

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/").get(warehouseController.warehouseList);
 router.route("/:id").get(warehouseController.warehouseSingle);
+router.route("/:id/inventories").get(warehouseController.inventoryByWarehouse);
 
 export default router;
