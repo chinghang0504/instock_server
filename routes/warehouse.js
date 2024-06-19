ddd69a80fa34e3a822c64609e4c1773fa08fc123
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-19 10:43:08 -0700

    fix route path

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -4,12 +4,15 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 const router = express.Router();
 
 router.route("/")
-.get(warehouseController.warehouseList)
-.post(warehouseController.warehouseCreate)
-.put(warehouseController.warehouseEdit);
+    .get(warehouseController.warehouseList)
+    .post(warehouseController.warehouseCreate)
+    .put(warehouseController.warehouseEdit);
 
-router.route("/:id").get(warehouseController.warehouseSingle)
-.delete(warehouseController.warehouseDelete)
-.get(warehouseController.inventoryByWarehouse);
+router.route("/:id")
+    .get(warehouseController.warehouseSingle)
+    .delete(warehouseController.warehouseDelete)
+
+router.route("/:id/inventories")
+    .get(warehouseController.inventoryByWarehouse);
 
 export default router;
